f896be12e39eb948e4b75af8a1e39e50785639b8
Edward Kelly <ekelly206@gmail.com>
2023-08-10 12:40:38 -0400

    SubmitForm working with backend

--- vue/src/components/CakeDetail.vue
+++ vue/src/components/CakeDetail.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class = "cake-box">  
-    <h2>{{cake.title}}</h2>
+  <div class="cake-box">
+    <h2>{{ cake.title }}</h2>
     <img class="cake-image" :src="cake.image" />
-    <h3>{{cake.description}} </h3>
-    <h3>{{cake.style}} </h3>
-    <h3>$ {{cake.price}}</h3>
+    <h3>{{ cake.description }}</h3>
+    <h3>{{ cake.style }}</h3>
+    <h3>$ {{ cake.price }}</h3>
     <a
       id="show-form-button"
       href="#"
@@ -12,7 +12,7 @@
       v-if="showForm === false"
       >Place An Order</a
     >
-    <form v-on:submit.prevent="sumbitOrder" v-if="showForm === true">
+    <form v-on:submit.prevent="submitOrder" v-if="showForm === true">
       <div class="form-element">
         <label for="firstName">First Name: </label>
         <input id="firstName" type="text" v-model="newOrder.firstName" />
@@ -31,7 +31,12 @@
       </div>
       <div class="form-element">
         <label for="streetName">Street Name: </label>
-        <input id="streetName" placeholder="Street Name" type="text" v-model="newOrder.streetName" />
+        <input
+          id="streetName"
+          placeholder="Street Name"
+          type="text"
+          v-model="newOrder.streetName"
+        />
       </div>
       <div class="form-element">
         <label for="city">City: </label>
@@ -45,78 +50,83 @@
         <label for="zipCode">Zip Code: </label>
         <input id="zipCode" type="text" v-model="newOrder.zip" />
       </div>
-      <p>Would you like to add writing to your cake? (+5$) <input type="checkbox" v-on:change="displayTextArea()"/>
+      <p>
+        Would you like to add writing to your cake? (+5$)
+        <input type="checkbox" v-on:change="displayTextArea()" />
       </p>
       <div class="form-element" v-if="showTextArea === true">
         <label for="cakeWriting">Writing to Appear on cake: </label>
-        <textarea id="cakeWriting" rows="8" cols="60" v-model="newOrder.writing" />
+        <textarea
+          id="cakeWriting"
+          rows="8"
+          cols="60"
+          v-model="newOrder.writing"
+        />
       </div>
       <div class="actions" v-if="showForm === true">
         <button v-on:click="resetForm" type="button">Cancel</button>
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </div>
     </form>
   </div>
 </template>
 
 <script>
-import CakeService from '../services/CakeService';
-
+import CakeService from "../services/CakeService";
 
 export default {
- data() {
-       return {
-         showForm: false,
-         showTextArea: false,
-         cake : {
-         cake_id: "",
-         title: "",
-         description: "",
-         price: "",
-         style: "",
-         size: "",
-         flavor: "",
-         frosting: "",
-         filling:"",
-         availability: "",
-         image: ""
-                 },
-        newOrder:{
-           
-
-         }               
-    }
-    },
-
-    created() { //$route.params.cake_id
-        //this.cake= this.$store.state.cake
-        CakeService.getCake(this.$route.params.id)
-        .then((response) => 
-        {this.cake = response.data
-        this.newOrder.price = this.cake.price
-        this.newOrder.cake_id = this.cake.cake_id}).catch((error) => alert(error))
-
-    },
-    methods: {
-      submitOrder() {
-        const Order = this.newOrder
-        CakeService.placeOrder(Order).then(response => {
-          if (response.status === 201) {
-            this.$router.push({name: 'OrderConfirmation', params: {orderid:response.data.order_id}})
-          }
-
-        })
+  data() {
+    return {
+      showForm: false,
+      showTextArea: false,
+      cake: {
+        cake_id: "",
+        title: "",
+        description: "",
+        price: "",
+        style: "",
+        size: "",
+        flavor: "",
+        frosting: "",
+        filling: "",
+        availability: "",
+        image: "",
       },
-      displayTextArea() {
-        this.showTextArea =! this.showTextArea
-      },
-      resetForm() {
-        this.showForm = false;
-        this.showTextArea = false;
-        this.newOrder = {}
-      }
-    }
-}
+      newOrder: {},
+    };
+  },
+
+  created() {
+    //$route.params.cake_id
+    //this.cake= this.$store.state.cake
+    CakeService.getCake(this.$route.params.id)
+      .then((response) => {
+        this.cake = response.data;
+      })
+      .catch((error) => alert(error));
+  },
+  methods: {
+    submitOrder() {
+      this.newOrder.price = this.cake.price;
+      this.newOrder.cake_id = this.cake.cake_id;
+      CakeService.placeOrder(this.newOrder).then((response) => {
+          this.$router.push({
+            name: "OrderConfirmation",
+            params: { orderid: response.data.order_id },
+          });
+        
+      });
+    },
+    displayTextArea() {
+      this.showTextArea = !this.showTextArea;
+    },
+    resetForm() {
+      this.showForm = false;
+      this.showTextArea = false;
+      this.newOrder = {};
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -129,15 +139,14 @@ export default {
   flex-direction: column;
   align-items: center;
   border: 1px solid #ccc; /* Add a border around the box */
-  border-radius: 10px;    /* Rounded edges */
-  padding: 20px 10px;     /* Vertical padding: 20px, Horizontal padding: 10px */
+  border-radius: 10px; /* Rounded edges */
+  padding: 20px 10px; /* Vertical padding: 20px, Horizontal padding: 10px */
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* Add a subtle box shadow */
   background-color: #fff; /* Background color for the box */
-  overflow: hidden;       /* Hide any content that overflows */
-  background-image: url('https://img.freepik.com/free-vector/blank-leafy-frame-social-ads_53876-100923.jpg?w=2000');
+  overflow: hidden; /* Hide any content that overflows */
+  background-image: url("https://img.freepik.com/free-vector/blank-leafy-frame-social-ads_53876-100923.jpg?w=2000");
 }
 div.form-element {
   margin-top: 10px;
 }
-
 </style>
