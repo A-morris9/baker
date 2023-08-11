653794d9be0859a28e5daddc8159a0388d2aa563
Stefan Kyntchev <stefan.kyntchev@techelevator.com>
2023-08-11 10:43:32 -0400

    added cards

--- vue/src/components/CakeCard.vue
+++ vue/src/components/CakeCard.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="cake" v-on:click="viewCake(cake)">
-      <h2 class="cake-name" style ="grid-area: name">{{ cake.title }}</h2>
-       
-      <img class="cake-image" style = "grid-area: image" :src="cake.image"/>
-  </div>
+        <div class="carousel-item">
+          <img class="carousel-item__img" :src="cake.image"/>
+          <div class="carousel-item__details">
+            <div class="controls">
+              <i class="fa fa-birthday-cake" style="font-size:20px;color:white;"></i>
+            </div>
+            <h5 class="carousel-item__details--title">{{ cake.title }}</h5>
+            <h6 class="carousel-item__details--subtitle">Date and Duration</h6>
+          </div>
+        </div>
 </template>
 
 <script>
@@ -25,29 +30,88 @@ export default {
 </script>
 
 <style>
-.cake {
-    font-family:'Big Shoulders Display', cursive;
-    display: grid;
-    grid-template-rows: 1fr 1fr;
-    grid-template-areas:
-    "name" 
-    "image";
-    border: 1px solid rgb(119, 114, 114);
-    border-radius: 10px;
-    width: 170px;
-    height: 230px;
-    margin: 20px auto;
-    justify-items: center;
-    align-items: center;
-    text-align: center;
-    cursor: pointer;
-     box-shadow: 0px 6px 8px rgba(0, 0, 0, 0.1);
-    background-color: bisque;
+
+.carousel-item {
+  width: 200px;
+  height: 250px;
+  border-radius: 10px;
+  background-color: #95bcd6;
+  overflow: hidden;
+  margin-right: 10px;
+  margin-top: 70px;
+  display: inline-block;
+  cursor: pointer;
+  -webkit-transition: 1000ms all;
+  transition: 1000ms all;
+  -webkit-transform-origin: center left;
+  transform-origin: center left;
+  position: relative;
 }
-.cake-image {
-    width: 70%;
-    height: 80%;
-    margin-top: -10px
+
+.carousel-item:hover ~ .carousel-item {
+  -webkit-transform: translate3d(100px, 0, 0);
+  transform: translate3d(100px, 0, 0);
+}
+
+.carousel__container:hover .carousel-item {
+  opacity: 0.3;
+}
+
+.carousel__container:hover .carousel-item:hover {
+  -webkit-transform: scale(1.5);
+  transform: scale(1.5);
+  opacity: 1;
+}
+
+.carousel-item__img {
+  width: 200px;
+  height: 250px;
+  -o-object-fit: cover;
+  object-fit: cover;
+}
+
+.carousel-item__details {
+  background: -webkit-gradient(linear, left bottom, left top,
+    from(rgba(0, 0, 0, 0.9)),
+    to(rgba(0, 0, 0, 0))
+  );
+  background: linear-gradient(
+    to top,
+    rgba(0, 0, 0, 0.9) 0%,
+    rgba(0, 0, 0, 0) 100%
+  );
+  font-size: 10px;
+  opacity: 0;
+  -webkit-transition: 450ms opacity;
+  transition: 450ms opacity;
+  padding: 10px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.carousel-item__details:hover {
+  opacity: 1;
+}
+
+.carousel-item__details span {
+  /* width: 10px;
+  height: 10px; */
+  font-size: 0.9rem;
+  color: #2ecc71;
+  /* background-color: white; */
+}
+
+.carousel-item__details .controls {
+  padding-top: 180px;
+}
+
+.carousel-item__details .carousel-item__details--title,
+.carousel-item__details--subtitle {
+  color: #fff;
+  margin: 5px 0;
 }
 
 </style>
--- vue/src/components/CakeList.vue
+++ vue/src/components/CakeList.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class = "fancy-background">
+  <div class = "fancy-background carousel">
   
     <hr class = "divider"/> 
     <hr class = "divider" />
     Filter cake name by: <input type="text" v-model="filter" class="fancy" />
-   <div class="cakes">
+   <div class="carousel__container">
     <cake-card v-for="cake in filteredCakes" v-bind:key="cake.cake_id" :cake="cake" class="cake-card"></cake-card>
    </div>
   </div> 
@@ -43,23 +43,23 @@ export default {
 </script>
 
 <style>
- .cakes {  
-  display: flex; 
-  flex-wrap : wrap;
-  justify-content: space-around;
-  align-items: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  padding: 20px;
-  gap:100px
+
+.carousel {
+  width: 100%;
+  overflow-x: scroll;
+  padding: 30px;
+  padding-top: 80px;
+  position: relative;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
 }
-.cake-card {
-  width: calc(250px - 20px); /* 20% width with 20px spacing */
-  height: 300px;
-  margin-bottom: 20px; /* Add some bottom margin for spacing */
-  justify-content: center;
+
+.carousel__container {
+  margin: 70px 0px;
+  padding-bottom: 10px;
+  display: inline-block;
 }
+
 .fancy-background {
   background-image: url('https://img.freepik.com/free-vector/blank-leafy-frame-social-ads_53876-100923.jpg?w=2000');
 }
@@ -69,6 +69,5 @@ export default {
 }
 .divider {
   border: none; /* Remove the border */
-  
 }
 </style>
