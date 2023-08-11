8c94e06d71cbc414773596fdcf869df1d3e9f85d
Edward Kelly <ekelly206@gmail.com>
2023-08-11 15:45:08 -0400

    day 5 eod

--- vue/src/components/OrderStatusBaker.vue
+++ vue/src/components/OrderStatusBaker.vue
@@ -1,25 +1,34 @@
 <template>
  <div>
-  <h1>this is an order status page</h1>
-  <input type="text" v-model="orderid"/>
-  <button @click="findOrderDetails" class="button">Retrieve Order Details</button>
+  <h1>this is an order list page for bakers only</h1>
   <table>
       <thead>
         <tr>
-          <th>Order Number</th>
-          <th>Customer Name</th>
-          <th>Pickup Date</th>
-          <th>Cake Style</th>
-          <th>Cake Size</th>
-          <th>Cake Flavor</th>
-          <th>Cake Frosting</th>
-          <th>Cake Filling</th>
+          <th>Order ID</th>
+          <th>Cake ID</th>
+          <th>Last Name</th>
+          <th>Phone Number/th>
+          <th>Order Date</th>
+          <th>Pick-Up Date</th>
+          <th>Writing</th>
+          <th>Price</th>
+          <th>Phone Number</th>
+          <th>Status</th>
         </tr>
       </thead>
-      <tbody>
-          <td>
-              <label v-for="order in listOfOrders" v-bind:key="order.order_id"></label>
-          </td>
+      <tbody> 
+         <tr v-for="order in listOfOrders" v-bind:key="order.order_id"> 
+              <td>{{order.order_id}}</td>  
+              <td>{{order.cake_id}}</td>
+              <td>{{order.lastName}}</td>
+              <td>{{order.phoneNumber}}</td>
+              <td>{{order.orderDate}}</td>
+              <td>{{order.pickupDate}}</td>
+              <td>{{order.writing}}</td>
+              <td>{{order.totalAmount}}</td>
+              <td>{{order.phoneNumber}}</td>
+              <td>{{order.status}}</td>  
+         </tr>
       </tbody>
     </table>   
  </div> 
@@ -27,7 +36,6 @@
 
 <script>
 import OrderService from "../services/OrderService";
-import CakeService from "../services/CakeService";
 
 export default {
     data() {
@@ -44,16 +52,7 @@ export default {
         })
     },
     methods:{
-        findOrderDetails() {
-        OrderService.getOrder(this.orderid).then((response) => {
-            this.order = response.data
-            this.findCakeDetails();
-        })},
-        findCakeDetails() {
-        CakeService.getCake(this.order.cake_id).then((response) => {
-            this.cake = response.data
-        }) 
-        }
+
     } 
 
     }
