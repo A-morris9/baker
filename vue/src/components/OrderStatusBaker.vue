76a37e1b7e82ca738de9dbe7db4dfa5947c59728
Matthew <mchristian1987@icloud.com>
2023-08-15 14:13:33 -0400

    Added Calender

--- vue/src/components/OrderStatusBaker.vue
+++ vue/src/components/OrderStatusBaker.vue
@@ -1,66 +1,81 @@
 <template>
- <div>
-  <h1>this is an order list page for bakers only</h1>
-  <label class = "style" for="filter"> Filter Orders by Current Order Status: </label>
-  <input type="text" v-model="filter" />
-  <table>
+  <div>
+    <h1>this is an order list page for bakers only</h1>
+    <full-calendar :events="fcEvents" locale="en"></full-calendar>
+    <label class="style" for="filter">
+      Filter Orders by Current Order Status:
+    </label>
+    <input type="text" v-model="filter" />
+    <table>
       <thead>
         <tr>
-          <th> Order ID |</th> 
-          <th> Cake ID |</th> 
-          <th> Last Name |</th> 
-          <th> Phone Number |</th> 
-          <th> Order Date |</th> 
-          <th> Pick-Up Date |</th> 
-          <th> Writing |</th> 
-          <th> Price |</th> 
-          <th> Phone Number |</th> 
-          <th> Current Order Status |</th> 
-          <th> Change Order Status </th>
+          <th>Order ID |</th>
+          <th>Cake ID |</th>
+          <th>Last Name |</th>
+          <th>Phone Number |</th>
+          <th>Order Date |</th>
+          <th>Pick-Up Date |</th>
+          <th>Writing |</th>
+          <th>Price |</th>
+          <th>Phone Number |</th>
+          <th>Current Order Status |</th>
+          <th>Change Order Status</th>
         </tr>
       </thead>
-      <tbody> 
-         <tr v-for="order in filteredOrders" v-bind:key="order.order_id"> 
-              <td>{{order.order_id}}</td>  
-              <td>{{order.cake_id}}</td>
-              <td>{{order.lastName}}</td>
-              <td>{{order.phoneNumber}}</td>
-              <td>{{formatDate(order.orderDate)}}</td>
-              <td>{{formatDate(order.pickupDate)}}</td>
-              <td>{{order.writing}}</td>
-              <td>{{order.totalAmount}}</td>
-              <td>{{order.phoneNumber}}</td>
-              <td>{{order.status}}</td>
-              <td>
-                <select @change="updateOrderStatus(order, $event.target.value)">
-                 <option value="">Select Status</option>
-                 <option value="Pending">Pending</option>
-                 <option value="Canceled">Canceled</option>
-                 <option value="Ready">Ready</option>
-                 <option value="Complete">Complete</option>
-                </select>
-              </td>
-         </tr>
+      <tbody>
+        <tr v-for="order in filteredOrders" v-bind:key="order.order_id">
+          <td>{{ order.order_id }}</td>
+          <td>{{ order.cake_id }}</td>
+          <td>{{ order.lastName }}</td>
+          <td>{{ order.phoneNumber }}</td>
+          <td>{{ formatDate(order.orderDate) }}</td>
+          <td>{{ formatDate(order.pickupDate) }}</td>
+          <td>{{ order.writing }}</td>
+          <td>{{ order.totalAmount }}</td>
+          <td>{{ order.phoneNumber }}</td>
+          <td>{{ order.status }}</td>
+          <td>
+            <select @change="updateOrderStatus(order, $event.target.value)">
+              <option value="">Select Status</option>
+              <option value="Pending">Pending</option>
+              <option value="Canceled">Canceled</option>
+              <option value="Ready">Ready</option>
+              <option value="Complete">Complete</option>
+            </select>
+          </td>
+        </tr>
       </tbody>
-    </table>   
- </div> 
+    </table>
+  </div>
 </template>
 
 <script>
 import OrderService from "../services/OrderService";
 import {parseISO, format} from "date-fns";
 
+ let events = [
+	{
+      title : 'We got cakes',
+      start : '2023-08-25',
+      end : '2023-08-27'
+    }
+]
 
 export default {
     data() {
     return {
+      fcEvents: events,
         filter : "",
         listOfOrders : [],
     }
     },
+    components:{
+      'full-calendar':require('vue-fullcalendar')
+    },
     created() {
         OrderService.getOrders().then((response) => {
             this.listOfOrders = response.data;
+            this.populateFcEvents();
             this.sortOrders();
         })
         
@@ -77,8 +92,16 @@ export default {
       formatDate(dateStr){
         const date = parseISO(dateStr);
         return format(date, "MM/dd/yyyy");
-      }
-
+      },
+        populateFcEvents() {
+    this.fcEvents = this.listOfOrders.map((order) => {
+      return {
+        title: `Order #${order.order_id}, ${order.lastName}`,
+        start: order.orderDate,   // Assuming orderDate is in a valid format
+        end: order.pickupDate,    // Assuming pickupDate is in a valid format
+      };
+    });
+  },
       },
      computed: {
       filteredOrders() {
@@ -88,15 +111,16 @@ export default {
         );
       } else return this.listOfOrders;
     },
-     } 
-    } 
+   } 
+ } 
 
   
 </script>
 
 <style scoped>
-h1, th {
-  font-family: 'Big Shoulders Display', cursive;
+h1,
+th {
+  font-family: "Big Shoulders Display", cursive;
   color: rgb(80, 71, 66);
 }
 th {
@@ -109,5 +133,4 @@ th {
 .style {
   color: rgb(80, 71, 66);
 }
-
 </style>
